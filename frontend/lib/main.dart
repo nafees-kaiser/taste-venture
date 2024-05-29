ea297bdafa0f75b21e0610a2ad2830f4cd9f25cb
Shahabuddin1122 <shavoddin54@gmail.com>
2024-05-29 21:04:02 +0600

    add personal information page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/pages/MyApp.dart';
+import 'package:frontend/pages/Profile.dart';
 
 void main() {
   runApp(MaterialApp(
     initialRoute: '/',
     routes: {
-      '/': (context) => const MyApp(),
+      '/': (context) => const Profile(),
     },
   ));
 }
