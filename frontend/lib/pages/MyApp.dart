15907d68d85e2c571b59e1fdac1afcb5aca462c0
Nafees Kaiser <oyonafees2001@gmail.com>
2024-06-01 10:18:49 +0600

    Add textbox in registration screen

--- frontend/lib/pages/MyApp.dart
+++ frontend/lib/pages/MyApp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/registration_customer.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Updated Demo Home Page'),
+      home: RegistrationCustomer(),
     );
   }
 }
