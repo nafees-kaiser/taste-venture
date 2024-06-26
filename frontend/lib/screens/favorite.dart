47766063cc45f92c918a20fa8aa770d87bdebd21
RafsanProve <124572117+RafsanProve@users.noreply.github.com>
2024-06-26 19:36:25 +0600

    Add FavCard widget

--- frontend/lib/screens/favorite.dart
+++ frontend/lib/screens/favorite.dart
@@ -1,63 +1,33 @@
 ﻿import 'package:flutter/material.dart';
+import 'package:frontend/widgets/FavCard.dart';
 
-class Favorite extends StatelessWidget {
+List<> data = [];
+
+class Favorite extends StatefulWidget {
   const Favorite({super.key});
 
+  @override
+  State<Favorite> createState() => _FavoriteState();
+}
+
+class _FavoriteState extends State<Favorite> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: const Text("Favorites"),
         ),
-        body: SingleChildScrollView(
+        body: const SingleChildScrollView(
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  width: double.infinity,
-                  height: 240,
-                  padding: const EdgeInsets.fromLTRB(25, 5, 25, 5),
-                  child: Card.outlined(
-                    borderOnForeground: true,
-                    clipBehavior: Clip.hardEdge,
-                    shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                    ),
-                    color: Colors.white,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(20),
-                          child: Image.asset(
-                            'assets/JeansCakes.png',
-                            width: 328,
-                            height: 150,
-                            fit: BoxFit.fitWidth,
-                          ),
-                        ),
-                        Padding(
-                          padding:
-                              const EdgeInsets.fromLTRB(0.0, 4.0, 0.0, 1.0),
-                          child: Text("Favorite 1",
-                              style: TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              )),
-                        ),
-                        Text("Favorite 2",
-                            style: TextStyle(
-                              fontSize: 16,
-                            )),
-                      ],
-                    ),
-                  ),
-                )
+                FavCard()
               ],
             ),
           ),
         ));
   }
 }
+
