8f4932efba8bda9772b642dea37ef7dc71886515
Syed Nafees Kaiser <oyonafees2001@gmail.com>
2024-09-10 03:28:11 +0600

    Modify tour spot add feature

--- Backend/usersapp/serializers.py
+++ Backend/usersapp/serializers.py
@@ -3,6 +3,7 @@ from rest_framework import serializers
 from common.serializers import AppUserSerializer
 from .models import *
 from common.models import AppUser
+from common.utils import *
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -14,34 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         user = validated_data.pop('user')
-        app_user = AppUser.objects.create(**user)
-        app_user.set_password(user['password'])
-        app_user.save()
+        app_user = create_common_user(user)
         customer = Users.objects.create(user=app_user, **validated_data)
         return customer
 
     def to_internal_value(self, data):
-        user = {
-            'name': data.pop('name'),
-            'email': data.pop('email'),
-            'password': data.pop('password'),
-            'address': data.pop('address'),
-            'contact': data.pop('contact'),
-            'user_type': 'customer'
-        }
-        data['user'] = user
-        return super().to_internal_value(data)
+        new_data = add_user(data, 'customer')
+        return super().to_internal_value(new_data)
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
-        user_representation = {
-            "name": instance.user.name,
-            "email": instance.user.email,
-            "address": instance.user.address,
-            "contact": instance.user.contact,
-            "user_type": instance.user.user_type,
-        }
-
+        user_representation = represent_user(instance.user)
         representation.update(user_representation)
         representation.pop('user')
         return representation
