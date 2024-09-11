12b3fca14b2c63a59d4f2facf5d094d8e3692a57
RafsanProve <124572117+RafsanProve@users.noreply.github.com>
2024-09-11 15:31:54 +0600

    Update views.py

--- Backend/tourspot/views.py
+++ Backend/tourspot/views.py
@@ -83,7 +83,10 @@ def accept_booking(request):
     try:
         user = Users.objects.get(id=request.data['user_id'])
         tourspot = Tourspot.objects.get(id=request.data['tourspot_id'])
-        booking = Booking.objects.get(user=user, tourspot=tourspot, date=request.data['date'])
+        booking = Booking.objects.get(user=user,
+                                    tourspot=tourspot,
+                                    date=request.data['date'],
+                                    status="pending")
         setattr(booking, 'status', "accepted")
         setattr(booking, 'message', request.data['message'])
         booking.save()
@@ -97,7 +100,10 @@ def reject_booking(request):
     try:
         user = Users.objects.get(id=request.data['user_id'])
         tourspot = Tourspot.objects.get(id=request.data['tourspot_id'])
-        booking = Booking.objects.get(user=user, tourspot=tourspot, date=request.data['date'])
+        booking = Booking.objects.get(user=user,
+                                    tourspot=tourspot,
+                                    date=request.data['date'],
+                                    status="pending")
         setattr(booking, 'status', "rejected")
         setattr(booking, 'message', request.data['message'])
         booking.save()
